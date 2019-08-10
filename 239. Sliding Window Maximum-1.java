bd0c0da006afb70e57d696aebd4304736cf5fa5a
Jacob <zixuan.bai@pku.edu.cn>
2019-08-11 00:25:14 +0800

    new file:   239. Sliding Window Maximum-1.java 	modified:   239. Sliding Window Maximum-1.java 	modified:   703. Kth Largest Element in a Stream.java

---
+++ 239. Sliding Window Maximum-1.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        if(nums==null||nums.length==0||k==1){
+            return nums;
+        }
+        
+        PriorityQueue<Integer> pq = new PriorityQueue<>((o1,o2)->o2-o1);
+        int[] res = new int[nums.length-k+1];
+        
+        for(int i=0;i<k;i++){
+            pq.add(nums[i]);
+        }
+        res[0]=pq.peek();
+        
+        for(int i=1;i<res.length;i++){
+            pq.remove(nums[i-1]);
+            pq.add(nums[i-1+k]);
+            res[i]=pq.peek();
+        }
+        return res;
+    }
+}
